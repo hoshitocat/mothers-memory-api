2c4ac8acc85e34ea5aeb9f028eddde0b933fa09f
d0rocy <hoshitocat@gmail.com>
2017-12-10 05:00:26 +0900

    :sparkles: can create new task

--- app.rb
+++ app.rb
@@ -2,11 +2,29 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pry'
 Dir["./models/*.rb"].each { |f| require f }
 
 set :bind, '0.0.0.0'
 
-get '/' do
-  @tasks = Task.tasks
-  { tasks: @tasks }.to_json
+get '/tasks' do
+  tasks = Task.tasks
+  { tasks: tasks }.to_json
+end
+
+patch '/tasks/:id' do
+  'hoge'
+end
+
+post '/tasks' do
+  json = request.body.read
+  parse_params = JSON.parse(json).symbolize_keys
+  task = Task.new(title: parse_params[:title], notification_date: parse_params[:notification_date], user_id: Task::DEFAULT_USER_ID)
+  if task.save
+    status 200
+    body task.to_json
+  else
+    status 422
+    body task.errors.full_messages
+  end
 end
